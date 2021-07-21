fd3d9ea3d332d4b99429e64828147a7e3ee081ae
bidhan <yamk1029@gmail.com>
2021-07-21 09:11:31 +0545

    owlcarousel added

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@ $(window).on('load',function(){
  $('#status').delay(2000).fadeOut();
  $('#preloader').delay(2500).fadeOut();
 });
+
+$(document).ready(function () {
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots:false,
+        navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>']
+    });
+
+}); 
