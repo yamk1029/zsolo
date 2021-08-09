8dd88819478b465148c59f1686f5f15df37c404a
bidhan <yamk1029@gmail.com>
2021-08-09 19:47:12 +0545

    testimonial add

--- js/script.js
+++ js/script.js
@@ -15,7 +15,30 @@ $(document).ready(function () {
         navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>']
     });
 }); 
-
 $('#services-tabs').responsiveTabs({
     startCollapsed: 'accordion'
+});
+$(window).on('load', function () {
+    $('#isotope-container').isotope({});
+    $('#isotope-filters').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        $('#isotope-container').isotope({
+            filter: filterValue
+        });
+        $('#isotope-filters').find('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+}); 
+
+$(document).ready(function () {
+    $("#testimonials-slider").owlCarousel({
+        items: 1,
+        autoplay: false,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>']
+    });
 });
